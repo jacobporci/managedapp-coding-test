314aee0d1abc08ad269c4e58d62cb391f7d6a7ec
Jacob Porciuncula <jacobporci@gmail.com>
2022-11-21 11:47:26 +0800

    Add test for validate; Add more test cases for getDaysBetween

--- index.test.js
+++ index.test.js
@@ -1,13 +1,28 @@
-const { getDaysBetween, getDaysBetweenJSDateUtil } = require("./utils.js");
+const {
+  getDaysBetween,
+  getDaysBetweenJSDateUtil,
+  validate,
+} = require("./utils.js");
 
-test("get same output as JS Date/time utils", () => {
-  [
-    ["2/6/1983", "22/6/1983"],
-    ["4/7/1984", "25/12/1984"],
-    ["3/1/1989", "3/8/1983"],
-  ].forEach(([date1, date2]) => {
-    expect(getDaysBetween(date1, date2)).toEqual(
-      getDaysBetweenJSDateUtil(date1, date2)
-    );
+describe("getDaysBetween", () => {
+  it("should get same output as JS Date/Time utils", () => {
+    [
+      ["1/1/2000", "1/1/2001"],
+      ["2/6/1983", "22/6/1983"],
+      ["4/7/1984", "25/12/1984"],
+      ["3/1/1989", "3/8/1983"],
+    ].forEach(([date1, date2]) => {
+      expect(getDaysBetween(date1, date2)).toEqual(
+        getDaysBetweenJSDateUtil(date1, date2)
+      );
+    });
+  });
+});
+
+describe("validate", () => {
+  it("should return false if date is invalid", () => {
+    ["1", "a", "$", "1/1/1899", "1/1/3000"].forEach((date) => {
+      expect(validate(date)).not.toBeTruthy();
+    });
   });
 });
